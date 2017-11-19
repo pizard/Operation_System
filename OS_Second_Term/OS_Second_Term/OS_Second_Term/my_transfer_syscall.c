135383583d41287209756ec3818631cb33a7ffae
pizard <pho0987@naver.com>
2017-11-20 00:36:37 +0900

    [Operation_System][SecondTerm][프로젝트파일]

---
+++ OS_Second_Term/OS_Second_Term/OS_Second_Term/my_transfer_syscall.c
@@ -0,0 +1,25 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+
+
+extern int faster_PID;
+extern int slower_PID;
+
+asmlinkage long sys_benchmark_F(int pid) {	  // 318
+
+	faster_PID = pid;
+
+	return 0;
+
+};
+EXPORT_SYMBOL(faster_PID);
+
+
+asmlinkage long		sys_benchmark_S(int pid) {	// 319
+
+	slower_PID = pid;
+
+	
+	return 0;
+};
+EXPORT_SYMBOL(slower_PID);
